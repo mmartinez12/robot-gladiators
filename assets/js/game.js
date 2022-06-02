3e88e83363a481b2032a8b4db2c48a73d53fb805
Meg Martinez <meg.music.martinez@gmail.com>
2022-06-02 13:25:30 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,8 +4,14 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 var enemyNames = ['Roborto', 'Amy Android', 'Robo Trumble'];
-var enemyHealth = 50;
-var enemyAttack = 12;
+var enemyHealth = randomNumber(40, 60);
+var enemyAttack = 10;
+
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  return value;
+};
 
 // fight function (now with parameter for enemy's name) 
 var fight = function(enemyName) {
@@ -22,14 +28,16 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney)
         break;
       }
     }
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+   var damage = randomNumber(playerAttack - 3, playerAttack);
+   
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -48,7 +56,9 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -163,7 +173,7 @@ var shop = function() {
       playerMoney = playerMoney - 7;
       }
       else {
-        window.alert{"You don't have enough money!"}
+        window.alert("You don't have enough money!")
       }
       break;
 
